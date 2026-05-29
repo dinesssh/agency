2f5c6efd186ad0814d957d9a505cd11240de3ff6
dinesssh <dineshkumardk13506@gmail.com>
2026-05-29 11:03:46 +0530

    Fix GitHub Pages deployment and workflow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = 'agency';
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  images: {
+    unoptimized: true,
+  },
   turbopack: {
     root: ".",
   },
